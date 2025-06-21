046596d002f657f14ab1f8bf9255b3eb29a26f7a
harrisskuy <harisudesu03@gmail.com>
2025-06-21 10:39:29 +0700

    Create admin.blade.php

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container mt-4">
+    <h1 class="text-primary">Welcome to SoftBlue Panel</h1>
+    @yield('subcontent')
+</div>
+@endsection
